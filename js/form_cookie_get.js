9af2d3d189e146f74312e7d903a5f1dc1ed54f7a
Ryan Ramphal <rramphal94@gmail.com>
2016-06-08 23:52:35 -0400

    Final changes made

--- js/form_cookie_get.js
+++ js/form_cookie_get.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var name = Cookies.get("name");
 	if(name){
 		alert("Your name is " + name + "!");
-		$("#info").animate({left: "+=500"}, 2000);
+		$("#info").css({color: blue}, 2000);
 	}
 	else {
 		alert("Cookie's value was not set! You need to fill out the form first!");
--- js/form_cookie_set.js
+++ js/form_cookie_set.js
@@ -1,34 +1,5 @@
 function validateForm(){
-	var name = document.getElementById('username').value();
+	var name = document.getElementById('name').value;
 	Cookies.set("name", name);
-}
-
-$("#form").on("submit", function(e){
-	e.preventDefault();
-	var search_term = document.getElementById("term").value;
-	var search_type = document.getElementById("type").value;
-	alert(search_term+" "+search_type);
-	var returned = $.ajax({
-		type: "GET",
-		url: "https://api.spotify.com/v1/search",
-		data: {
-		q: search_term,
-		type: search_type
-		},
-		success: function (response) {
-			console.log(response);
-			var data = JSON.parse(response);
-			document.getElementById("output").innerHTML="";
-			for(var i=0; i<data.artists.length; i++){
-				for(var j=0; j<data.artists[i].items.length; j++){
-					console.log(data.artists[i].items);
-					for(var k=0; k<data.artists[i].items[j].images.length; k++){
-						console.log(data.artists[i].items[j].images);
-						document.getElementById("output").innerHTML += "<img src='" + response.artists[i].items[j].images[k].url + "'>";
-					}
-				}
-			}
-		}
-	});
 	return false;
-});
+}
